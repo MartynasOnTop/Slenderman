85e23066ece6e5447af10ef1e3e34c55957dd421
Martynas <135633452+MartynasOnTop@users.noreply.github.com>
2023-12-17 18:37:10 +0200

    lights sounds

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Flashlight : MonoBehaviour
+{
+    public bool isOn;
+    public Light light;
+
+    int battery = 100;
+    public TMP_Text percent;
+
+    public AudioSource click;
+
+    private void Start()
+    {
+        InvokeRepeating("BatteryDown", 3f, 3f);
+    }
+    private void Update()
+    {
+        light.enabled = isOn;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isOn = !isOn;
+
+            click.Play();
+        }
+        if (battery <= 0)
+        {
+            light.enabled = false;
+            percent.text = "0";
+        }
+        else if (battery <= 10)
+        {
+            isOn = !isOn;
+        }
+    }
+    void BatteryDown()
+    {
+        if (isOn)
+        {
+            battery -= 2;
+            percent.text = battery.ToString();
+        }
+        if (battery >= 70)
+        {
+            percent.color = Color.green;
+            light.range = 40;
+            light.intensity = 3;
+        }
+        else if (battery >= 30)
+        {
+            percent.color = Color.yellow;
+            light.range = 30;
+            light.intensity = 2.5f;
+        }
+        else
+        {
+            percent.color = Color.red;
+            light.range = 20;
+            light.intensity = 2;
+        }
+    }
+}
